484773a735f0888a52e486af0172e7ee1eeaddbf
kirlar <oguzhan.kirlar@adesso.com.tr>
2023-08-29 12:46:47 +0300

    test

---
+++ src/main/java/com/example/icontest/IconTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.icontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IconTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/icontest/IconTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
